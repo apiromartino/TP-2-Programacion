627c427e81ae08145a23ea10f3705a82361179dc
Ariii1996 <ariel.piro@hotmail.com>
2016-04-21 14:37:52 -0300

    Update Codigo.pas

--- Codigo.pas
+++ Codigo.pas
@@ -17,6 +17,47 @@ type
 	tvNomDjs=array [tiDjs] of string[40];
 	tmTemasxDj=array [TiDjs,tiTemasxDj] of tvNomTemas;
 
+
+Procedure IngreseListaTemas(var nombretemas:tvNomTemas; var durtemas:tmDurTemas);     
+
+
+
+
+
+
+Procedure IngreseListaDjs();
+
+
+
+
+
+
+
+Procedure IngreseListaTemasxDj();
+
+
+Procedure Menu1(var nombretemas:tvNomTemas; var durtemas:tmDurTemas; var nomDjs:tvNomDjs; var temasxDj:tmTemasxDj );
+const 
+     OPMAX=1;
+     OPMIN=3;
+var
+     opcionmen1:byte;
+begin
+     writeln('Ingrese la opción deseada');
+     writeln('1- Ingresar lista de temas');
+     writeln('2- Ingresar lista de Djs');
+     writeln('3- Ingresat temas que va a tocar cada Dj');
+     repeat
+        readln(opcionmen1);
+        case opcionmen1 of
+            1: IngreseListaTemas(nombretemas,durtemas);
+            2: IngreseListaDjs(nomDjs);
+            3: IngreseListaTemasxDj(temasxDj,MLdj,MLtem);
+        else writeln('Ingreso una opción inválida, vuelva a elegir una opción');    
+        end;
+     until ((opcionmen1>=OPMIN) and (opcionmen<=OPMAX));   
+end;
+
 var 
     MLdj:tiDjs; {maximo logico de DJs, no se si se va a necesitar, por ahi inicializando en 0 es mejor}
 	MLtem:tiTemasxDj; {maximo logico de temas, no se si se va a necesitar, por ahi inicializando en 0 es mejor}
@@ -24,15 +65,9 @@ var
 	durtemas:tmDurTemas;
 	nomDjs:tvNomDjs;
 	temasxDj:tmTemasxDj;
-	opcionmen1:byte;
-     
-Procedure
-
-
 
 BEGIN 
-    IngreseListaTemas(nombretemas,durtemas);
-    IngreseListaDjs(nomDjs);
-    IngreseListaTemasxDj(temaszDj,MLdj,MLtem);
+    Inicializarmatriztemasxdj(temasxDj){si inicializamos esta matiz en 0 creo que no son necesarios los ML}
+    Menu1(
 
 END.
