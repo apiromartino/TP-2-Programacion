20a4dd4eab6daeb1c9d15bf5c21e5c04aea82e3e
Ariii1996 <ariel.piro@hotmail.com>
2016-04-21 12:54:27 -0300

    Update Codigo.pas

--- Codigo.pas
+++ Codigo.pas
@@ -1,38 +1,38 @@
 program DjsTemas;
 
 const 
-
-	MAXDjs=25;
-	MAXTemasxDj=35;
-	MAXTEM=200;
-	MAXMIN=10;
+	MAXDjs=25; 
+	MAXTemasxDj=35; 
+	MAXTEM=10; {Ahora tiene 10 para cuando probamos no tener que poner los 200 temas, despues hay que cambiarlo} 
+	MAXMIN=10; 
 	MAXSEGS=59;
 
-
 type
-
-	tiNombre=1..20; {No se si va a ser necesario este indice para hacer que nos e pase ni el nombre de la cancion ni el nombre de los djs de 20}
 	tiDjs=1..MAXDjs;
 	tiTemasxDj=1..MAXTemasxDj;
 	tiTemas=1..MAXTEM;
 	tiMinSeg=(minutos,segundos);
-	tvNombre1=array [tiNombre] of char; {este seria el vector con 20 caracteres para el nombre de las canciones}
-	tvNombre2=array [tiNombre] of char;{este seria el vector con 20 caracteres para el nombre de los Djs}
-	tvNomTemas=array [tiTemas] of Tvnombre1;
+	tvNomTemas=array [tiTemas] of string[20];
 	tmDurTemas=array [tiTemas,tiMinSeg] of byte;
-	tvNomDjs=array [tiDjs] of tvNombre2;
+	tvNomDjs=array [tiDjs] of string[40];
 	tmTemasxDj=array [TiDjs,tiTemasxDj] of tvNomTemas;
 
-var
-
-	MLdj:tiDjs; }
-	MLtem:tiTemasxDj; 
+var 
+    MLdj:tiDjs; {maximo logico de DJs, no se si se va a necesitar, por ahi inicializando en 0 es mejor}
+	MLtem:tiTemasxDj; {maximo logico de temas, no se si se va a necesitar, por ahi inicializando en 0 es mejor}
 	nombretemas:tvNomTemas;
 	durtemas:tmDurTemas;
 	nomDjs:tvNomDjs;
 	temasxDj:tmTemasxDj;
+	opcionmen1:byte;
+     
+Procedure
 
 
-BEGIN IngresarListaDeTemas (
+
+BEGIN 
+    IngreseListaTemas(nombretemas,durtemas);
+    IngreseListaDjs(nomDjs);
+    IngreseListaTemasxDj(temaszDj,MLdj,MLtem);
 
 END.
