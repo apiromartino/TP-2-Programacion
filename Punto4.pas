1a6723de87f6a226509aa98a38b4e020f92414f3
Ariii1996 <ariel.piro@hotmail.com>
2016-04-26 19:54:53 -0300

    Create Punto4.pas

---
+++ Punto4.pas
@@ -0,0 +1,33 @@
+Procedure Inicializarvector (var temasrepetidos:tvTemasRepetidos);
+
+var
+	i:byte;
+begin
+	for i:=1 to MAXTEM do
+		temasrepetidos[i]:=0;
+end;
+
+Procedure TemasMasTocados(temasPorDj:tmTemasPorDj; listaTemas:tmListaTemas; temasRepetidos:tvTemasRepetidos; MLDjs:tiDjs); {AGREGAR TIPOS}
+var i:byte;
+	j:byte;
+	k:byte;
+	maximoRepetidos:byte;
+
+begin
+	Inicializarvector(temasRepetidos);
+	for k:=1 to MLDjs do
+		while temasPorDj[k,i]<>'0' do
+			begin
+				for j:=1 to MAXTEM do
+					if (temasPorDj[k, i]=listaTemas[j,nombre]) then
+						temasRepetidos[j]:= temasRepetidos[j] + 1;
+				i:=i+1;
+			end;		
+	maximoRepetidos:=0;
+	for i:=1 to MAXTEM do
+		if (temasRepetidos[i]>maximoRepetidos) then
+			maximoRepetidos:=temasRepetidos[i];
+	for i:=1 to MAXTEM do
+		if temasRepetidos[i]=maximoRepetidos then
+			writeln(listaTemas[i,nombre], ' es de los mas repetidos, esta repetido ', maximoRepetidos, ' veces');
+end;		
